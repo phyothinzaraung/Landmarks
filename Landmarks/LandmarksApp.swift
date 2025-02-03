94598deb216778ce93e96691a46b382808361300
Phyo Thinzar Aung <phyothinzar.ag92@gmail.com>
2025-02-03 17:18:28 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Phyo Thinzar Aung on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
